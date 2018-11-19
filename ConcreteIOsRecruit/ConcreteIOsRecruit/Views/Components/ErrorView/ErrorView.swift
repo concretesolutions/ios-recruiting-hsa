fe140910c9bc967e55fca769fac5cfbca170e739
Matías Contreras <contreras2004@gmail.com>
2018-11-19 18:04:21 -0300

    starting to add the error message screen

--- ConcreteIOsRecruit/ConcreteIOsRecruit/Views/Components/ErrorView/ErrorView.swift
+++ ConcreteIOsRecruit/ConcreteIOsRecruit/Views/Components/ErrorView/ErrorView.swift
@@ -9,8 +9,41 @@
 import UIKit
 import Lottie
 
-class ErrorView: UIView {
+struct MessageData{
+    let message: String
+    let animationName: String
+}
+
+enum MessageType{
+    case noConnection
+    case genericError
+    case emptyFavorites
+    case noSearchResults
     
+    func getData() -> MessageData{
+        switch self{
+        case .noConnection:
+            return MessageData(message: "Sin conexión a internet", animationName: "no-connection")
+        case .genericError:
+            return MessageData(message: "Ups.. Ocurrió un error inesperado", animationName: "broken_stick_error")
+        case .emptyFavorites:
+            return MessageData(message: "Aquí encontrarás tus favoritos una vez que los agregues.", animationName: "no-connection")
+        case .noSearchResults:
+            return MessageData(message: "No se encontraron resultados", animationName: "no-connection")
+        }
+    }
+}
+
+class MessageView: UIView {
+    
+    @IBOutlet weak var animationCanvas: LOTAnimationView!
+    @IBOutlet weak var errorMessageLbl: UILabel!
+    var messageType: MessageType = .genericError {
+        didSet{
+            self.errorMessageLbl.text = messageType.getData().message
+            self.addAnimation(named: messageType.getData().animationName)
+        }
+    }
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
@@ -23,11 +56,14 @@ class ErrorView: UIView {
     
     private func commonInit(){
         fromNib()
-        
-        let animationView = LOTAnimationView(name: "broken_stick_error")
-        animationView.frame = self.frame
+    }
+    
+    func addAnimation(named: String){
+        let animationView = LOTAnimationView(name: named)
+        animationView.frame = animationCanvas.frame
         animationView.frame.origin = CGPoint.zero
-        self.addSubview(animationView)
+        animationCanvas.addSubview(animationView)
+        animationView.layoutAttachAll(to: animationCanvas)
         animationView.loopAnimation = true
         animationView.play()
     }
