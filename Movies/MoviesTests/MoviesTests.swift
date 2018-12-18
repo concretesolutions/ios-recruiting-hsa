aa60a2a8dd936960049ee57e91f808cfaa00b411
Maria Virginia Navas <maria.navasf@externos-cl.cencosud.com>
2018-12-18 18:06:50 -0300

    More Unit Tests

--- Movies/MoviesTests/MoviesTests.swift
+++ Movies/MoviesTests/MoviesTests.swift
@@ -11,30 +11,32 @@ import XCTest
 
 class MoviesTests: XCTestCase {
 
+    var testMovie: Movie? = nil
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        if DefaultsManager.shared.favoriteMovies.count > 0 {
+            testMovie = DefaultsManager.shared.favoriteMovies[0]
+        }
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
     
     func testGetFavoriteMovies(){
         DefaultsManager.shared.getFavorites()
         let favoriteMoviesArray = DefaultsManager.shared.favoriteMovies
         XCTAssert(favoriteMoviesArray.count > 0)
+    }
+    
+    func testSaveMovie(){
+        if var movieToSave = testMovie {
+            movieToSave.setFavorite()
+            XCTAssert(movieToSave.isFavorite ?? false)
+        } else {
+            XCTFail()
+        }
     }
 
 }
