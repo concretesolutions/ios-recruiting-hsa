f41c27fd34d3278f49bbd8b6bd5e0501b2a1518d
Diego Vargas <diego@yafun.cl>
2019-04-20 13:06:46 -0400

    carthage install

---
+++ recruiting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  recruiting
+//
+//  Created by Diego Vargas on 4/20/19.
+//  Copyright © 2019 concrete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
