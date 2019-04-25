c72649ef8bad519baa8dbd8c9410abe0d0a472c8
Audel Dugarte <>
2019-04-25 00:40:23 -0400

    first commit

---
+++ Concrete-Movies/Concrete-Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concrete-Movies
+//
+//  Created by Audel Dugarte on 4/24/19.
+//  Copyright © 2019 Audel Dugarte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
