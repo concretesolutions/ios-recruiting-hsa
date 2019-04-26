e09ef40bd95d6b1eea1e01d3422722f11d993989
Audel Dugarte <audel.dugarte@scotiabank.cl>
2019-04-26 17:06:57 -0400

    added cocoapods added alamofire added tabbar and navigationbar to appdelegate

--- Concrete-Movies/Concrete-Movies/AppDelegate.swift
+++ Concrete-Movies/Concrete-Movies/AppDelegate.swift
@@ -12,10 +12,61 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    lazy var greetingsNavigationController: UINavigationController = {
+        //let viewController = ViewControllerFactory.viewController(type: .greetings)
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.isNavigationBarHidden = true
+        return navigationController
+    }()
+    
+    func generateTabBarController()->UITabBarController{
+        let tabBarController = UITabBarController()
+        
+        let favoritesVC = ViewController()
+        favoritesVC.title = "Favorites"
+        favoritesVC.view.backgroundColor = UIColor.orange
+        let downloadsVC = ViewController()
+        downloadsVC.title = "Downloads"
+        downloadsVC.view.backgroundColor = UIColor.blue
+        let historyVC = ViewController()
+        historyVC.title = "History"
+        historyVC.view.backgroundColor = UIColor.cyan
+        
+        
+//        let button: UIButton = UIButton(frame: CGRect(x: view.bounds.width / 250, y: view.bounds.height / 2, width: 100, height: 50))
+//        button.backgroundColor = UIColor.black
+//        button.addTarget(self, action: #selector(pushToNextVC), for: .touchUpInside)
+//        self.view.addSubview(button)
+        
+        favoritesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 0)
+        downloadsVC.tabBarItem = UITabBarItem(tabBarSystemItem: .downloads, tag: 1)
+        historyVC.tabBarItem = UITabBarItem(tabBarSystemItem: .history, tag: 2)
+        
+        let controllers = [favoritesVC, downloadsVC, historyVC]
+        tabBarController.viewControllers = controllers.map { UINavigationController(rootViewController: $0)}
+        
+        return tabBarController
+    }
 
+    func pushToNextVC() {
+        let newVC = UIViewController()
+        newVC.view.backgroundColor = UIColor.red
+//        self.navigationController?.pushViewController(newVC, animated:
+//            true)
+    }
+
+    func prepareWindow() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        //window?.rootViewController = BackingViewController(rootViewController: greetingsNavigationController)
+        window?.rootViewController = generateTabBarController()
+        window?.makeKeyAndVisible()
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        prepareWindow()
         return true
     }
 
---
+++ Concrete-Movies/Concrete-Movies/Core/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  Concrete-Movies
+//
+//  Created by Audel Dugarte on 4/26/19.
+//  Copyright © 2019 Audel Dugarte. All rights reserved.
+//
+import UIKit
+
+enum Colors {
+    enum Primary {
+        static let brand: UIColor = #colorLiteral(red: 0.977786243, green: 0.8375340104, blue: 0.4414522946, alpha: 1) // (hex: "F7CE5E")
+        static let dark: UIColor = #colorLiteral(red: 0.1764705882, green: 0.1882352941, blue: 0.2784313725, alpha: 1) // (hex: "2D3047")
+        static let accent: UIColor = #colorLiteral(red: 0.8509803922, green: 0.5921568627, blue: 0.1176470588, alpha: 1) // (hex: "D9971E")
+        static let white: UIColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) // (hex: "FFFFFF")
+    }
+}
