16604af435937649e3b3bb644294bd63030096a4
Audel Dugarte <audel.dugarte@scotiabank.cl>
2019-04-29 19:25:58 -0400

    added realm pod

---
+++ Concrete-Movies/Concrete-Movies/Movies/Data/Entity/FavoritedMovieEntity.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritedMovieEntity.swift
+//  Concrete-Movies
+//
+//  Created by Audel Dugarte on 4/29/19.
+//  Copyright © 2019 Audel Dugarte. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class FavoritedMovieEntity: Object{
+    @objc dynamic var name = ""
+    @objc dynamic var movieId = 0
+    @objc dynamic var overview = ""
+    @objc dynamic var posterPath = ""
+}
