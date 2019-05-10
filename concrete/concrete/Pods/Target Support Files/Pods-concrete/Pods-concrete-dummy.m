1acd8c5d2ea51ac5b2a7b8a4e721f55ef93e151d
Andres Ortiz <andresdavidortiz@yahoo.com>
2019-05-09 21:26:28 -0400

    Adds pods and frameworks

---
+++ concrete/concrete/Pods/Target Support Files/Pods-concrete/Pods-concrete-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_concrete : NSObject
+@end
+@implementation PodsDummy_Pods_concrete
+@end
---
+++ concrete/concrete/Pods/Target Support Files/Pods-concreteTests/Pods-concreteTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_concreteTests : NSObject
+@end
+@implementation PodsDummy_Pods_concreteTests
+@end
