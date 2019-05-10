358f4694de2fb5a133415dd64f860d77e04fc1b9
Andres Ortiz <andresdavidortiz@yahoo.com>
2019-05-10 10:11:53 -0400

    Adds unit tests

--- concrete/concrete/concreteTests/concreteTests.swift
+++ concrete/concrete/concreteTests/concreteTests.swift
@@ -8,20 +8,77 @@
 
 import XCTest
 @testable import concrete
+import OHHTTPStubs
 
 class concreteTests: XCTestCase {
 
+    var loader : MTAMovieLoader?
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        loader = MTAMovieLoader.shared
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testRPIMovieLoaderPopular() {
+        
+        let successExpectation = self.expectation(description: "success called")
+        
+        stub(condition: isHost("api.themoviedb.org")) { _ in
+            let stubPath = OHPathForFile("wsresponsepopular.json", type(of: self))
+            return fixture(filePath: stubPath!, headers: ["Content-Type":"application/json"])
+        }
+        
+        loader?.fetchMovies(kind: 1, page: 1, text: ""){
+            (result: [Movie], success) in
+            XCTAssertEqual(20, result.count)
+            successExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    
+    func testRPIMovieLoaderTopRated() {
+        
+        let successExpectation = self.expectation(description: "success called")
+        
+        stub(condition: isHost("api.themoviedb.org")) { _ in
+            let stubPath = OHPathForFile("wsresponsetoprated.json", type(of: self))
+            return fixture(filePath: stubPath!, headers: ["Content-Type":"application/json"])
+        }
+        
+        loader?.fetchMovies(kind: 1, page: 1, text: ""){
+            (result: [Movie], success) in
+            let movie = result.first
+            XCTAssertEqual(movie?.title, "Dilwale Dulhania Le Jayenge")
+            successExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1, handler: nil)
+        
+    }
+    
+    func testRPIMovieLoaderMovieSearch() {
+        
+        let successExpectation = self.expectation(description: "success called")
+        
+        stub(condition: isHost("api.themoviedb.org")) { _ in
+            let stubPath = OHPathForFile("wsresponseserach.json", type(of: self))
+            return fixture(filePath: stubPath!, headers: ["Content-Type":"application/json"])
+        }
+        
+        loader?.fetchMovies(kind: 1, page: 1, text: "Captain Marvel"){
+            (result: [Movie], success) in
+            let movie = result.first
+            XCTAssertEqual(movie?.title, "Captain Marvel")
+            successExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1, handler: nil)
+        
     }
 
     func testPerformanceExample() {
