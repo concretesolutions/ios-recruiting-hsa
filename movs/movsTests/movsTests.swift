2defdf82c76780c74dc7664dc94cc6401e205c82
AndresMotion <andres@motiondisplays.cl>
2019-10-01 17:02:11 -0300

    - initial commit

---
+++ movs/movsTests/movsTests.swift
@@ -0,0 +1,34 @@
+//
+//  movsTests.swift
+//  movsTests
+//
+//  Created by Andrés Alexis Rivas Solorzano on 10/1/19.
+//  Copyright © 2019 Andrés Alexis Rivas Solorzano. All rights reserved.
+//
+
+import XCTest
+@testable import movs
+
+class movsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
