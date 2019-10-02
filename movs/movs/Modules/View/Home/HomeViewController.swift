a5fc51dde3ce0bdd7acb7ef859519490a55ccec9
AndresMotion <andres@motiondisplays.cl>
2019-10-02 10:02:42 -0300

    - Safe commit

---
+++ movs/movs/Modules/View/Home/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  movs
+//
+//  Created by Andrés Alexis Rivas Solorzano on 10/2/19.
+//  Copyright © 2019 Andrés Alexis Rivas Solorzano. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
