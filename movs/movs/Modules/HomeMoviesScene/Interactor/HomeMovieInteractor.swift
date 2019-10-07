ad01adbec465c17354cbd3669ff86383f0ee842a
AndresMotion <andres@motiondisplays.cl>
2019-10-07 00:25:26 -0300

    - safecommit

--- movs/movs/Modules/HomeMoviesScene/Interactor/HomeMovieInteractor.swift
+++
@@ -1,9 +0,0 @@
-//
-//  HomeMovieInteractor.swift
-//  movs
-//
-//  Created by Andrés Alexis Rivas Solorzano on 10/2/19.
-//  Copyright © 2019 Andrés Alexis Rivas Solorzano. All rights reserved.
-//
-
-import Foundation
