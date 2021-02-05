37e22f92e06f5be5933b08bf4bc07c0c3b80468d
Kristian Sthefan Cortes Prieto <SoapyGenie@SoapyGenie.local>
2021-02-04 21:52:19 -0300

    (master): first commit

---
+++ concreteIOSRecruitChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  concreteIOSRecruitChallenge
+//
+//  Created by Kristian Sthefan Cortes Prieto on 04-02-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
