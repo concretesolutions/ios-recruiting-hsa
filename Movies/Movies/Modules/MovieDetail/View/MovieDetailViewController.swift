0fc9fb6e0c0ce4d8b47d8c3bf243145f59762286
alfredolucomav <alfredo.luco@mavericks.cool>
2021-02-05 22:28:07 -0300

    capa ui de detalle de pelicula

--- Movies/Movies/Modules/MovieDetail/View/MovieDetailViewController.swift
+++ Movies/Movies/Modules/MovieDetail/View/MovieDetailViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class MovieDetailViewController: ViewController {
     
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var mainStackView: UIStackView!
+    
     //MARK: - Memory debug
     
     deinit {
@@ -18,15 +28,43 @@ class MovieDetailViewController: ViewController {
     //MARK: - Variables
     
     var presenter: MovieDetailPresenter = MovieDetailPresenter()
+    var movie: Movie = Movie()
 
     //MARK: - App lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.presenter.fetchCategories()
+        setupUI()
+        self.activityIndicator.startAnimating()
         self.presenter.fetchMovie()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
+    //MARK: - Functions
+    
+    func setupUI() {
+        mainStackView.setCustomSpacing(24.0, after: thumb)
+        mainStackView.setCustomSpacing(8.0, after: nameLabel)
+        nameLabel.textColor = .black
+        nameLabel.font = UIFont.systemFont(ofSize: 32.0)
+        mainStackView.setCustomSpacing(8.0, after: yearLabel)
+        yearLabel.textColor = .black
+        yearLabel.font = UIFont.systemFont(ofSize: 16.0)
+        mainStackView.setCustomSpacing(8.0, after: genreLabel)
+        genreLabel.textColor = .black
+        genreLabel.font = UIFont.systemFont(ofSize: 16.0)
+        summaryLabel.textColor = .lightGray
+        summaryLabel.font = UIFont.systemFont(ofSize: 16.0)
     }
 
 }
@@ -35,11 +73,24 @@ class MovieDetailViewController: ViewController {
 
 extension MovieDetailViewController: PresenterToViewMovieDetailProtocol {
     func fetchMovieSuccessfull(_ movie: Movie) {
-        
+        self.movie = movie
+        self.nameLabel.text = movie.title
+        if let url = URL(string: Constants.mediaURL + "/original/\(movie.poster_path ?? "")") {
+            thumb.load(url: url)
+        }
+        self.yearLabel.text = String(movie.release_date?.split(separator: "-").first ?? "")
+        self.summaryLabel.text = movie.overview
+        self.presenter.fetchCategories()
     }
     
     func fetchCategoriesSuccessfull(_ categories: [Category]) {
-        
+        self.activityIndicator.stopAnimating()
+        self.activityIndicator.isHidden = true
+        let arr = categories.filter { (cat) -> Bool in
+            return (self.movie.genre_ids?.contains(where: { $0 == cat.id })) ?? false
+        }.compactMap({ $0.name })
+        let str = arr.joined(separator: ",")
+        self.genreLabel.text = str
     }
     
     func failure(_ error: Error) {
