7c13a0a9cd8bc8f890e7f521b254077cda22a9fb
alfredolucomav <alfredo.luco@mavericks.cool>
2021-02-07 17:30:32 -0300

    ui layer

--- Movies/Movies/Modules/FilterContent/View/FilterContentViewController.swift
+++ Movies/Movies/Modules/FilterContent/View/FilterContentViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class FilterContentViewController: ViewController {
     
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     //MARK: - Memory debug
     
     deinit {
@@ -18,21 +22,51 @@ class FilterContentViewController: ViewController {
     //MARK: - Variables
     
     var presenter: FilterContentPresenter = FilterContentPresenter()
+    var content: [String] = []
     
     //MARK: - App lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupUI()
+        self.presenter.fetchContent()
         // Do any additional setup after loading the view.
     }
+    
+    //MARK: - Functions
+    
+    func setupUI() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
 
+}
+
+//MARK: - UITableView Delegate & Data Source
+
+extension FilterContentViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return content.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .white
+        cell.textLabel?.textColor = .black
+        cell.textLabel?.text = self.content[indexPath.row]
+        return cell
+    }
 }
 
 //MARK: - Display Logic
 extension FilterContentViewController: PresenterToViewFilterContentProtocol {
     func fetchContentSuccessfull(_ content: [String]) {
-        
+        self.content = content
+        self.tableView.reloadData()
     }
     
     func failure(_ error: Error) {
