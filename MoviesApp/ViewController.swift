0544b87633b4466d8c62c3fd1340665c2859eac5
Hector Morales <hjmh.17@gmail.com>
2021-02-27 15:54:39 -0300

    initial commit

---
+++ MoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Hector Morales on 2/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
