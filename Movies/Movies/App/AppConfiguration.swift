6ca3e5218a2d744470ba29c4b0344e2066bd7c63
Daniel Nuñez <daniel.nunez@tinet.cl>
2021-03-04 12:36:12 -0300

    API endpoints
    class with full path url

---
+++ Movies/Movies/App/AppConfiguration.swift
@@ -0,0 +1,29 @@
+//
+//  AppConfiguration.swift
+//  Movies
+//
+//  Created by Daniel Nunez on 04-03-21.
+//
+
+import Foundation
+
+final class AppConfiguration {
+    lazy var apiKey: String = {
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "ApiKey") as? String else {
+            fatalError("ApiKey must not be empty in plist")
+        }
+        return apiKey
+    }()
+    lazy var apiBaseURL: String = {
+        guard let apiBaseURL = Bundle.main.object(forInfoDictionaryKey: "ApiBaseURL") as? String else {
+            fatalError("ApiBaseURL must not be empty in plist")
+        }
+        return apiBaseURL
+    }()
+    lazy var imagesBaseURL: String = {
+        guard let imageBaseURL = Bundle.main.object(forInfoDictionaryKey: "ImageBaseURL") as? String else {
+            fatalError("ApiBaseURL must not be empty in plist")
+        }
+        return imageBaseURL
+    }()
+}
---
+++ Movies/Movies/Network/ApiEndpoints.swift
@@ -0,0 +1,29 @@
+//
+//  ApiEndpoints.swift
+//  Movies
+//
+//  Created by Daniel Nunez on 04-03-21.
+//
+
+import Foundation
+
+class API {
+
+    static func moviesPath(with page: Int) -> String {
+        let appConfiguration = AppConfiguration()
+        let base = appConfiguration.apiBaseURL
+        let key = appConfiguration.apiKey
+        let path = "\(base)/movie/popular?api_key=\(key)&language=es-ES&page=\(page)"
+        return path
+    }
+
+    static func genresPath(with page: Int) -> String {
+        let appConfiguration = AppConfiguration()
+        let base = appConfiguration.apiBaseURL
+        let key = appConfiguration.apiKey
+        let path = "\(base)/genre/movie/list?api_key=\(key)&language=es-ES"
+
+        return path
+    }
+
+}
