529c09030513922c316a066d729059a4225a25a6
Daniel Nuñez <daniel.nunez@tinet.cl>
2021-03-05 01:12:55 -0300

    FavoriteScene
    #Implement tableview on favorite controller
    #Refactor Favorites controller
    #Favorite Cell and xib

--- Movies/Movies/Presentation/FavoritesList/View/FavoritesController.swift
+++ Movies/Movies/Presentation/FavoritesList/View/FavoritesController.swift
@@ -7,25 +7,66 @@
 
 import UIKit
 
-class FavoritesController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+protocol FavoriteProtocol: class {
+
+    func load(_ movies: [Movie])
+}
+
+class FavoritesController: UIViewController, FavoriteProtocol {
 
     @IBOutlet weak var search: UISearchBar!
     @IBOutlet weak var table: UITableView!
     
     weak var coordinator: MainCoordinator?
-    
+    lazy var items: [Movie] = []
+    var presenter: FavoritePresenter?
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        presenter?.load()
     }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        0
+    func setupTable(){
+        table.register(FavoriteTableViewCell.nib, forCellReuseIdentifier: FavoriteTableViewCell.identifier)
+
+    }
+
+    func load(_ movies: [Movie]) {
+        items = movies
+    }
+
+
+}
+
+// MARK: - extension - UITableViewDataSource
+
+extension FavoritesController: UITableViewDataSource {
+    func numberOfSections(in _: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        return items.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
         return UITableViewCell()
     }
+}
 
+// MARK: - extension - UITableViewDelegate
+
+extension FavoritesController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle,
+                   forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let movie = items[indexPath.row]
+            presenter?.deleteFavorite(movie: movie)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
 }
