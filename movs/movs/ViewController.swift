c19f7830d21ccb766ad68d4ada45c98138cbe844
Carlos Petit <ca.alberto.p@gmail.com>
2021-03-13 15:01:57 -0300

    Create xcode project

---
+++ movs/movs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movs
+//
+//  Created by Carlos Petit on 13-03-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
