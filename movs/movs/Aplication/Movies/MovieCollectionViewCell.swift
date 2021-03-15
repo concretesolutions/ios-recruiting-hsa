e1f4c9067e2b824808e361a2041f1238ce9047fe
Carlos Petit <ca.alberto.p@gmail.com>
2021-03-15 20:04:24 -0300

    change story boards and update extensions and splash image

--- movs/movs/Aplication/Movies/MovieCollectionViewCell.swift
+++ movs/movs/Aplication/Movies/MovieCollectionViewCell.swift
@@ -8,5 +8,15 @@
 import UIKit
 
 class MovieCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var MovieImageViewCell: UIImageView!
+    @IBOutlet weak var MovieTitleViewCell: UILabel!
+    var numberRow = 0
+
     
+    func setUp(with movie: MovieViewModel, indexPath: Int){
+        MovieImageViewCell.load(url: movie.image)
+        MovieImageViewCell.contentMode = .scaleToFill
+        MovieTitleViewCell.text = movie.title
+        MovieTitleViewCell.adjustsFontSizeToFitWidth = true
+    }
 }
--- movs/movs/Aplication/Movies/MoviesViewController.swift
+++ movs/movs/Aplication/Movies/MoviesViewController.swift
@@ -8,22 +8,95 @@
 import UIKit
 
 class MoviesViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBAction func FavButtonPress(_ sender: UIButton) {
+        viewModel.favorite(at: Int(sender.tag))
+        let isFavorite = viewModel.item(at: Int(sender.tag)).favorite
+        sender.setImage(UIImage(systemName: isFavorite ? "heart.fill":"heart"), for: .normal)
+    }
+    var favButton = UIButton(frame: CGRect(x: 151, y: 214, width: 37, height: 30))
+    lazy var searchBar:UISearchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: 400, height: 20))
+    let activityView = UIActivityIndicatorView(style: .whiteLarge)
+    private var viewModel = MoviesViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        viewModel.delegate = self
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+        let leftNavBarButton = UIBarButtonItem(customView: searchBar)
+        
+        searchBar.placeholder = "Busqueda"
+        self.navigationItem.title = ""
+        self.navigationItem.leftBarButtonItem = leftNavBarButton
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        collectionView.reloadData()
     }
-    */
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+}
 
+
+extension MoviesViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let count = viewModel.numbersOfItems
+        return count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCollectionViewCell", for: indexPath) as? MovieCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        let largeConfig = UIImage.SymbolConfiguration(pointSize: 16, weight: .bold, scale: .large)
+        self.favButton = UIButton(frame: CGRect(x: 151, y: 214, width: 37, height: 30))
+        let isFavorite = viewModel.item(at: indexPath).favorite
+        self.favButton.setImage(UIImage(systemName: isFavorite ? "heart.fill":"heart", withConfiguration: largeConfig), for: .normal)
+        cell.setUp(with: viewModel.item(at: indexPath), indexPath: indexPath.row)
+       
+        
+        self.favButton.tintColor = UIColor.white
+        self.favButton.addTarget(self, action: #selector(FavButtonPress), for: UIControl.Event.touchUpInside)
+        self.favButton.tag = indexPath.row
+        self.favButton.isUserInteractionEnabled = true
+
+        cell.addSubview(self.favButton)
+        return cell
+    }
+}
+
+extension MoviesViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 190, height: 250)
+    }
+}
+
+extension MoviesViewController: MoviesViewModeldelegate{
+    func reloadData() {
+        collectionView.reloadData()
+    }
+}
+
+
+extension MoviesViewController: UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.searchBar.endEditing(true)
+        let sampleStoryBoard : UIStoryboard = UIStoryboard(name: "MovieStoryBoard", bundle:nil)
+        let detailMovieViewController  = sampleStoryBoard.instantiateViewController(identifier: "DetailMovieViewController") as! DetailMovieViewController
+        detailMovieViewController.selectedMovieIndex = indexPath.row
+        detailMovieViewController.selectedMovie = viewModel.item(at: indexPath)
+        detailMovieViewController.ModelView = viewModel
+        self.navigationController?.pushViewController(detailMovieViewController, animated: true)
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        self.searchBar.endEditing(true)
+    }
 }
