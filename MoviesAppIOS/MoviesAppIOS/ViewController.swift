31b06f54772fb5011595cafafe6143615905b67a
AlejandraVergaraMer <alejandraestrella1509@gmail.com>
2022-06-24 16:48:44 -0400

    Creación de proyecto
    Se crea el proyecto y se hace la e integración de biblioteca Alamofire por medio de gestor de dependencias CocoaPods

---
+++ MoviesAppIOS/MoviesAppIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesAppIOS
+//
+//  Created by Training on 24-06-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
