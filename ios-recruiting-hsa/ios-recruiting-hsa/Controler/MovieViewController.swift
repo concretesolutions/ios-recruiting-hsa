f7772117792957b908215986df8688ee50e86c12
cosmiqu3 <107438895+cosmiqu3@users.noreply.github.com>
2022-06-30 00:29:55 -0400

    Se incorpora Alamofire e iconos

---
+++ ios-recruiting-hsa/ios-recruiting-hsa/Controler/MovieViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MovieViewController.swift
+//  ios-recruiting-hsa
+//
+//  Created by training on 29-06-22.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ios-recruiting-hsa/ios-recruiting-hsa/Service/API.swift
@@ -0,0 +1,14 @@
+//
+//  API.swift
+//  ios-recruiting-hsa
+//
+//  Created by training on 30-06-22.
+//
+
+import Foundation
+import Alamofire
+
+class APiService{
+    
+    
+}
