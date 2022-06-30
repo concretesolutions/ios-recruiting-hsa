22ba489e8b68d0ac74feab23e2cda8f143821daf
cosmiqu3 <107438895+cosmiqu3@users.noreply.github.com>
2022-06-29 21:10:43 -0400

    Proyecto Base

---
+++ ios-recruiting-hsa/ios-recruiting-hsa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-recruiting-hsa
+//
+//  Created by training on 29-06-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
