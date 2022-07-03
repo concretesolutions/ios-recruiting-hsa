be1a6388aef68c941d299fffcc3ff2f4d8868ae2
jpradon <106857945+jpradon@users.noreply.github.com>
2022-07-03 01:01:57 -0400

    versión inicial

---
+++ movie-app-hsa/movie-app-hsa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movie-app-hsa
+//
+//  Created by training on 03-07-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ movie-app-hsa/movie-app-hsaUITests/movie_app_hsaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  movie_app_hsaUITestsLaunchTests.swift
+//  movie-app-hsaUITests
+//
+//  Created by training on 03-07-22.
+//
+
+import XCTest
+
+class movie_app_hsaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
