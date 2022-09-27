3511fe1a0293db4c45c088b897520bac65b790f8
Francisco ZD <fr.zunigad@gmail.com>
2022-09-26 21:55:19 -0300

    initial commit

---
+++ movieFinder/movieFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movieFinder
+//
+//  Created by Francisco Zuniga De Spirito on 26-09-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
