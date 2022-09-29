8603fd7eef393b6e079b9726cb0a88f93b0886b1
Francisco ZD <fr.zunigad@gmail.com>
2022-09-28 22:32:03 -0300

    add appIcon and reusable icons as assets

---
+++ movieFinder/movieFinder/Data/MovieItem.swift
@@ -0,0 +1,17 @@
+//
+//  Movie.swift
+//  movieFinder
+//
+//  Created by Francisco Zuniga De Spirito on 28-09-22.
+//
+
+import Foundation
+
+struct Movie {
+    let id: UUID
+    let imageURL: String
+    let name: String
+    let releaseDate: String
+    let genres: [String]
+    let description : String
+}
---
+++ movieFinder/movieFinder/Data/MovieLoader.swift
@@ -0,0 +1,8 @@
+//
+//  MovieLoader.swift
+//  movieFinder
+//
+//  Created by Francisco Zuniga De Spirito on 28-09-22.
+//
+
+import Foundation
